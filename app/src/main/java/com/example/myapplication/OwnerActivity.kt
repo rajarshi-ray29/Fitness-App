77bd8db7c4d82a3edc4a0d4f2c1da59568a02dc7
mr-harshtyagi <writetoharshtyagi@gmail.com>
2021-05-27 01:11:27 +0530

    Owner Side

--- app/src/main/java/com/example/myapplication/OwnerActivity.kt
+++ app/src/main/java/com/example/myapplication/OwnerActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,8 +21,10 @@ class OwnerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_owner)
         userRecyclerView = findViewById(R.id.recycler1)
+
         userRecyclerView.layoutManager = LinearLayoutManager(this)
         userRecyclerView.setHasFixedSize(true)
+
 
         userArrayList = arrayListOf<User>()
         getUserData()
--- app/src/main/java/com/example/myapplication/User.kt
+++ app/src/main/java/com/example/myapplication/User.kt
@@ -1,3 +1,7 @@
 package com.example.myapplication
 
+
 data class User(var name: String?=null, var gender: String?=null, var age: String?=null)
+{
+
+}
